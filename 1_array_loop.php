8b55c4cead4652e38209bd7bd5a02d7c94de5e62
TakeTheNoTe <thomas.lunar321@gmail.com>
2022-02-25 10:21:58 +0700

    1_array_loop-done

--- 1_array_loop.php
+++ 1_array_loop.php
@@ -1,6 +1,17 @@
 <?php
 $aplikasi[1]= 'gtAkademik';
-$aplikasi[1]= 'gtAkademik';
-$aplikasi[1]= 'gtAkademik';
-$aplikasi[1]= 'gtAkademik';
+$aplikasi[2]= 'gtFinansi';
+$aplikasi[3]= 'gtPerizinan';
+$aplikasi[4]= 'eCampuz';
+$aplikasi[5]= 'e0viz';
+
+$arrayLength = count($aplikasi);
+
+$i = 0;
+
+while ($i <= $arrayLength)
+        {
+            echo $aplikasi[$i];
+            $i++;
+        }
 ?>
