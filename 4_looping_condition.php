c0b4ddd4606a48f4c73eb12fa60fbad2d7cfa35b
TakeTheNoTe <thomas.lunar321@gmail.com>
2022-02-25 12:25:10 +0700

    init

--- 4_looping_condition.php
+++ 4_looping_condition.php
@@ -1,10 +1,10 @@
 <?php
 for ($i = 1; $i <= 50; $i++) {
-   if($i%7==0){
+   if($i%3==0){
        echo "Foo";
-   }elseif($i%5==0){
+   }if($i%5==0){
     echo "Bar";
-   }elseif($i%3==0&&$i%5==0){
+   }if($i%3==0&&$i%5==0){
     echo "FooBar";
    }
 }
